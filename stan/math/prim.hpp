2f7476d85bc454ed1ededab226b507140960cc50
andre <andre@cluster-local.node.drelen>
2019-05-21 14:56:37 -0400

    remove meta stuff

--- stan/math/prim.hpp
+++ stan/math/prim.hpp
@@ -1,28 +1,6 @@
 #ifndef STAN_MATH_PRIM_HPP
 #define STAN_MATH_PRIM_HPP
 
-#include <stan/math/prim/meta/get.hpp>
-#include <stan/math/prim/meta/index_type.hpp>
-#include <stan/math/prim/meta/is_vector.hpp>
-#include <stan/math/prim/meta/length.hpp>
-
-//#include <stan/math/prim/meta/as_array_or_scalar.hpp>
-//#include <stan/math/prim/meta/as_column_vector_or_scalar.hpp>
-//#include <stan/math/prim/meta/as_scalar.hpp>
-#include <stan/math/prim/meta/broadcast_array.hpp>
-#include <stan/math/prim/meta/get.hpp>
-#include <stan/math/prim/meta/index_type.hpp>
-#include <stan/math/prim/meta/is_constant_struct.hpp>
-#include <stan/math/prim/meta/is_vector.hpp>
-#include <stan/math/prim/meta/is_vector_like.hpp>
-#include <stan/math/prim/meta/length.hpp>
-#include <stan/math/prim/meta/length_mvt.hpp>
-#include <stan/math/prim/meta/operands_and_partials.hpp>
-#include <stan/math/prim/meta/scalar_type.hpp>
-#include <stan/math/prim/meta/seq_view.hpp>
-#include <stan/math/prim/meta/value_type.hpp>
-#include <stan/math/prim/meta/vector_seq_view.hpp>
-
 #include <stan/math/prim/err/check_cholesky_factor.hpp>
 #include <stan/math/prim/err/check_cholesky_factor_corr.hpp>
 #include <stan/math/prim/err/check_column_index.hpp>
