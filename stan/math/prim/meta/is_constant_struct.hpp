01709cee4e0c217e81d41bf46a73a8a4c663ed81
andre <andre@cluster-local.node.drelen>
2019-05-21 12:18:14 -0400

    add a bracket

--- stan/math/prim/meta/is_constant_struct.hpp
+++ stan/math/prim/meta/is_constant_struct.hpp
@@ -1,9 +1,7 @@
 #ifndef STAN_MATH_PRIM_SCAL_META_IS_CONSTANT_STRUCT_HPP
 #define STAN_MATH_PRIM_SCAL_META_IS_CONSTANT_STRUCT_HPP
-#include <stan/math/prim/meta/is_constant.hpp>
-#include <stan/math/prim/meta/is_constant_struct.hpp>
-#include <stan/math/prim/fun/Eigen.hpp>
-#include <stan/math/prim/meta/is_constant.hpp>
+#include <stan/math/prim.hpp>
+#include <stan/math/prim/meta.hpp>
 namespace stan {
 
 
@@ -44,4 +42,5 @@ struct is_constant_struct<Eigen::Block<T> > {
   enum { value = is_constant_struct<T>::value };
 };
 
+};
 #endif
