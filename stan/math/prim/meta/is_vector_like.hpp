9edca31d77fb1d479689f4298ec158605a3bb82d
andre <andre@cluster-local.node.drelen>
2019-05-21 10:04:25 -0400

    add bracket

--- stan/math/prim/meta/is_vector_like.hpp
+++ stan/math/prim/meta/is_vector_like.hpp
@@ -1,8 +1,9 @@
-#ifndef STAN_MATH_PRIM_SCAL_META_IS_VECTOR_LIKE_HPP
-#define STAN_MATH_PRIM_SCAL_META_IS_VECTOR_LIKE_HPP
-#include <stan/math/prim/meta/is_vector.hpp>
-#include <stan/math/prim/meta/is_vector_like.hpp>
-#include <stan/math/prim/fun/Eigen.hpp>
+#ifndef STAN_MATH_PRIM_META_IS_VECTOR_LIKE_HPP
+#define STAN_MATH_PRIM_META_IS_VECTOR_LIKE_HPP
+
+#include <stan/math/prim/meta.hpp>
+#include <stan/math/prim.hpp>
+
 namespace stan {
 
 
@@ -90,5 +91,6 @@ struct is_vector_like<Eigen::Matrix<T, R, C> > {
 template <typename T, int R, int C>
 struct is_vector_like<Eigen::Array<T, R, C> > {
   enum { value = true };
+};
 };
 #endif
